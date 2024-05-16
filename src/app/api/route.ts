4e5a3296704f86b7ac3853516231d0ea5202fb6a
최지원 <jiwonchoi@weedscomm.com>
2024-05-16 17:04:56 +0900

    Fix: 린트에러 수정 중

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,15 +4,17 @@ type ResponseData = {
 let data
 let isError = false
 
-export async function ENG_KOR_TRANSLATE(req: any) {
+export async function ENG_KOR_TRANSLATE(req: any, response: any) {
+    const header: any = {
+        'Content-Type': 'application/json',
+        'X-NCP-APIGW-API-KEY-ID': process.env.NEXT_PUBLIC_PAPAGO_ID,
+        'X-NCP-APIGW-API-KEY': process.env.NEXT_PUBLIC_PAPAGO,
+        'Access-Control-Allow-Origin': '*',
+    }
+    // @ts-ignore
     const res = await fetch('http://localhost:3000/v1/translation', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-NCP-APIGW-API-KEY-ID': process.env.NEXT_PUBLIC_PAPAGO_ID,
-            'X-NCP-APIGW-API-KEY': process.env.NEXT_PUBLIC_PAPAGO,
-            'Access-Control-Allow-Origin': '*',
-        },
+        headers: header,
         body: JSON.stringify(req)
     }).then((res) => {
         if (res.status !== 200) {
@@ -24,6 +26,7 @@ export async function ENG_KOR_TRANSLATE(req: any) {
     })
 
     if (!isError) {
+        // @ts-ignore
         const returnData = await res.json();
         data = returnData?.message.result;
     } else {
